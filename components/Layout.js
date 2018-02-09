1bd3bd733efc604aa996b56daf2f1fd11c8ddefb
rhiokim <rhio.kim@gmail.com>
2018-02-10 02:21:50 +0900

    feat: add nprogress plugin

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,16 @@
 // @flow
 import React from 'react'
 import Head from 'next/head'
-import { Nav, Header, Footer, Copyright } from './'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+import { Nav, Footer, Copyright } from './'
+
+Router.onRouteChangeStart = url => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 type Props = {
   children: any
@@ -16,6 +25,11 @@ export default class Layout extends React.Component<Props, {}> {
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="/static/css/nprogress.css"
           />
         </Head>
         <div id="wrapper">
