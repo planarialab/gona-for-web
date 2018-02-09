c29262150a85a7e9ab122c6edae5e9cc70787855
rhiokim <rhio.kim@gmail.com>
2018-02-10 00:31:03 +0900

    feat: add scss example and disabled cssModule

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
-  cssModules: true,
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
