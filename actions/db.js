fc89b7a711412d4b9485ed86d3bc68ee6e258e2a
rhiokim <rhio.kim@gmail.com>
2018-02-19 21:11:45 +0900

    feat: add PouchDB sample code

---
+++ actions/db.js
@@ -0,0 +1,5 @@
+// @flow
+
+import PouchDB from 'pouchdb'
+
+export default new PouchDB('http://localhost:5984/gona')
---
+++ pages/db/index.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+  <Link href="db/_info">
+    <a>db/_info</a>
+  </Link>
+)
